dc5d42aabf678c0f549731b909a331086c7fdbaa
Alireza-81 <alireza81.amiri@gmail.com>
2025-02-01 06:15:51 +0330

    added VL-T5

---
+++ VL-T5/target/debug/VLT5.d
@@ -0,0 +1,1 @@
+/workspace/VL-T5/target/debug/VLT5: /workspace/VL-T5/src/main.rs
---
+++ VL-T5/target/debug/deps/VLT5-dbf2c9205adb7525.d
@@ -0,0 +1,5 @@
+/workspace/VL-T5/target/debug/deps/VLT5-dbf2c9205adb7525: src/main.rs
+
+/workspace/VL-T5/target/debug/deps/VLT5-dbf2c9205adb7525.d: src/main.rs
+
+src/main.rs:
